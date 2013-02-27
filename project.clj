c1924590d18028150246eb9d06b4caa6da778716
Anthony Grimes <i@raynes.me>
2013-02-26 21:19:16 -0800

    Inc version.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject me.raynes/hickory "0.4.0"
+(defproject me.raynes/hickory "0.4.1"
   :description "HTML as Data"
   :url "http://github.com/Raynes/hickory"
   :license {:name "Eclipse Public License"
